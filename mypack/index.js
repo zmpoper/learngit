faa19053b2b3f46ee6d29445350732f569c7da36
zmpoper <51334314@qq.com>
2020-06-14 18:20:49 +0800

    跨域问题导致图书管理系统无法正常浏览

--- mypack/index.js
+++ mypack/index.js
@@ -44,19 +44,21 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 1337;
 
-//设置跨域访问
-app.all('*', function (req, res, next) {
+// 设置跨域访问 是为了方便VUE项目调用api接口针对json
+app.all('/api*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "content-type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1')
     res.header("Content-Type", "application/json;charset=utf-8");
+
     next();
 
 });
 
 //启动静态资源服务
 app.use(express.static('public', { index: 'login.html' }));
+// app.use(express.static('public'));
 //设置模版引擎
 //设置模版的路径
 app.set('views', path.join(__dirname, 'views'));
@@ -75,6 +77,7 @@ app.use(bodyParser.json());
 app.use(router);
 //监听端口
 app.listen(port, () => {
+    console.log('zhoumin');
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
